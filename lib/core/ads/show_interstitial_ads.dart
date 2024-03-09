5ac03245bc39f136629b8c99a1f73ed6ef323c96
Ashish <iamashishkoirala1@gmail.com>
2024-03-09 02:34:29 -0500

    ..

--- lib/core/ads/show_interstitial_ads.dart
+++ lib/core/ads/show_interstitial_ads.dart
@@ -1,22 +1,23 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void showInterstitialAd() {
-    InterstitialAd.load(
-      adUnitId: 'ca-app-pub-3940256099942544/8691691433',
-      request: const AdRequest(),
-      adLoadCallback: InterstitialAdLoadCallback(
-        onAdLoaded: (InterstitialAd ad) {
-          ad.fullScreenContentCallback = FullScreenContentCallback(
-            onAdDismissedFullScreenContent: (InterstitialAd ad) {
-              ad.dispose();
-            },
-          );
-          ad.show();
-        },
-        onAdFailedToLoad: (LoadAdError error) {
-          debugPrint('InterstitialAd failed to load: $error');
-        },
-      ),
-    );
-  }
+  InterstitialAd.load(
+    adUnitId: dotenv.env['AD_UNIT_ID'] ?? "",
+    request: const AdRequest(),
+    adLoadCallback: InterstitialAdLoadCallback(
+      onAdLoaded: (InterstitialAd ad) {
+        ad.fullScreenContentCallback = FullScreenContentCallback(
+          onAdDismissedFullScreenContent: (InterstitialAd ad) {
+            ad.dispose();
+          },
+        );
+        ad.show();
+      },
+      onAdFailedToLoad: (LoadAdError error) {
+        debugPrint('InterstitialAd failed to load: $error');
+      },
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:resume_builder/core/theme/custom_theme.dart';
 import 'package:resume_builder/features/authentication/data/data_source/remote_data_source.dart';
@@ -17,6 +18,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
     unawaited(MobileAds.instance.initialize());
+  // Load environment variables from .env file
+  await dotenv.load();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final prefs = await SharedPreferences.getInstance();
